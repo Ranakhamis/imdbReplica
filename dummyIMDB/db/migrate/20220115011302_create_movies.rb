49ed854d4885e5ca8f15320f3a33fe0ca6abb2f7
RanaKhamis <ranakhms@gmail.com>
2022-01-15 03:19:25 +0200

    Created Movie model

---
+++ dummyIMDB/db/migrate/20220115011302_create_movies.rb
@@ -0,0 +1,17 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.integer :rating
+      t.integer :awards
+      t.text :news
+      t.string :genre
+      t.string :director
+      t.string :actor
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
