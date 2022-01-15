26d67e26a957f42741ae73641f23c46c5c0810d7
RanaKhamis <ranakhms@gmail.com>
2022-01-15 02:25:53 +0200

    Facebook authentication and sign up

--- dummyIMDB/config/initializers/devise.rb
+++ dummyIMDB/config/initializers/devise.rb
@@ -271,6 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
+   
    config.omniauth :facebook, Rails.application.credentials.dig(:facebook, :facebook_client_id),
    Rails.application.credentials.dig(:facebook, :facebook_client_secret), scope: 'public_profile, email'
 
--- dummyIMDB/db/schema.rb
+++ dummyIMDB/db/schema.rb
@@ -12,15 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_01_14_200043) do
 
-  create_table "sessions", force: :cascade do |t|
-    t.string "session_id", null: false
-    t.text "data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -35,4 +26,15 @@ ActiveRecord::Schema.define(version: 2022_01_14_200043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
+ 
 end
