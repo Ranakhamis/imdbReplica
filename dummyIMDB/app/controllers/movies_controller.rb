bdc0095574dabd3bd589c03588140349fc328cf8
RanaKhamis <ranakhms@gmail.com>
2022-01-16 02:34:41 +0200

    Showed the movies by latest to user and Categorized by Genre

--- dummyIMDB/app/controllers/movies_controller.rb
+++ dummyIMDB/app/controllers/movies_controller.rb
@@ -1,7 +1,12 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.order(date: :desc) }
-    @movies = Movie.all.group_by {|m| m.genre }
+
+    @movies_categorized = Movie.all.group_by {|m| m.genre }
+
+    @movies_by_year = Movie.all.group_by {|m| m.date.year } 
+    
+    @movies = Movie.order(date: :desc) 
+
   end
 end
--- dummyIMDB/db/schema.rb
+++ dummyIMDB/db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_165651) do
+ActiveRecord::Schema.define(version: 2022_01_16_003151) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "rating"
     t.integer "awards"
     t.text "news"
     t.string "genre"
