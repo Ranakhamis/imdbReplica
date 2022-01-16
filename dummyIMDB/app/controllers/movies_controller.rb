107a16e194a7d23e404137745278c32ec6be5175
RanaKhamis <ranakhms@gmail.com>
2022-01-16 08:07:43 +0200

    Created actor model

--- dummyIMDB/app/controllers/movies_controller.rb
+++ dummyIMDB/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def index
-
+    
     @movies_categorized = Movie.all.group_by {|m| m.genre }
 
     @movies_by_year = Movie.all.group_by {|m| m.date.year } 
